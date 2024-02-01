ee795753f7c7132e9aa9ada4077b2f96dd60810b
Hasti <hasti.gondaliya@gmail.com>
2024-02-01 10:25:46 +0530

    Initial commit for nrf52840-dk as ble relay This commit includes to connect with esp32 as ble central The same connect with nrf connect mobile app as ble peripheral On request from mobile app, this nrf device read/write data from/to esp32 and forward to nrf connect app

---
+++ nrf52840-dk/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "C:/Users/sahil/Documents/Hasti/nrf52840-proj/central_and_peripheral_relay/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
